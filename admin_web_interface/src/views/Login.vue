f2c06c2355601a9e38b21e32a33f2fb2cb297019
Simon Knittel <hallo@simonknittel.de>
2019-11-07 19:54:30 +0100

    Add login form

--- admin_web_interface/src/views/Login.vue
+++ admin_web_interface/src/views/Login.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Home</h1>
+  <v-card class="mx-auto" max-width="344">
+    <v-card-title>Login</v-card-title>
+
+    <v-form>
+      <div class="px-4 pb-4">
+        <v-text-field label="Email address" type="email"></v-text-field>
+        <v-text-field label="Password" type="password"></v-text-field>
+
+        <div class="d-flex justify-end">
+          <v-btn color="deep-purple accent-4" dark>Submit</v-btn>
+        </div>
+      </div>
+    </v-form>
+  </v-card>
 </template>
